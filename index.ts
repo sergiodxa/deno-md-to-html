0633313fa7292c1b6110cdf11631b5ed5a197162
Sergio Xalambrí <hello@sergiodxa.com>
2020-05-13 20:41:52 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { readFileStr, writeFileStr } from "https://deno.land/std/fs/mod.ts";
 import { render } from "https://raw.github.com/srackham/rimu/master/mod.ts";
 
-const md = await readFileStr("./articles/swr/intro.mdx", { encoding: "utf8"})
-await writeFileStr("./index.html", render(md));
+const [input = "./index.md", output = "./index.html"] = Deno.args;
+const md = await readFileStr(input, { encoding: "utf8" });
+await writeFileStr(output, render(md));
